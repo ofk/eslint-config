dd3d273918084adc52b6138a882543d75cd062f7
ofk <ofkjpn+github@gmail.com>
2025-07-10 12:26:26 +0900

    Fix

--- src/configs/react-hooks.ts
+++ src/configs/react-hooks.ts
@@ -2,11 +2,5 @@ import pluginReactHooks from 'eslint-plugin-react-hooks';
 
 import { mergeRules } from '../utils';
 
-// see https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks#flat-config-eslintconfigjs
-export const reactHooksStrict = mergeRules(
-  // Flat Config version has not been released yet.
-  {
-    plugins: { 'react-hooks': pluginReactHooks },
-    rules: pluginReactHooks.configs.recommended.rules,
-  },
-);
+// see https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks#flat-config-eslintconfigjsts
+export const reactHooksStrict = mergeRules(pluginReactHooks.configs['recommended-latest']);
--- src/configs/react-refresh.ts
+++ src/configs/react-refresh.ts
@@ -3,4 +3,4 @@ import pluginReactRefresh from 'eslint-plugin-react-refresh';
 import { mergeRules } from '../utils';
 
 // see https://github.com/ArnaudBarre/eslint-plugin-react-refresh?tab=readme-ov-file#usage
-export const reactRefreshStrict = mergeRules(pluginReactRefresh.configs.recommended);
+export const reactRefreshStrict = mergeRules(pluginReactRefresh.configs.vite);
